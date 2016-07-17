b495033d820160481551ea26e6642d30358be731
Takuya Hasumi <zenith.de.quattro@gmail.com>
2016-07-17 08:18:00 +0000

    Add a full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,22 +2,33 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
   # 「Homeページのテスト。GETリクエストをhomeアクションに対して発行 (=送信) せよ。そうすれば、リクエストに対するレスポンスは[成功]になるはず。」
+  
+  def setup
+    @subtitle = "Ruby on Rails Tutorial Sample App"
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "#{@subtitle}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@subtitle}"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{@subtitle}"
+  end
+  
+ test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@subtitle}"
   end
 
 end
